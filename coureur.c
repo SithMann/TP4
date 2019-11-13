9c345d8d767e061a5f0d84293f3a5e7f24ea3785
SithMann <38142935+SithMann@users.noreply.github.com>
2019-11-13 15:11:50 +0100

    Add files via upload

---
+++ coureur.c
@@ -0,0 +1,74 @@
+#include "messages.h"
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <signal.h>
+
+/* VARIABLES GLOBALES (utilisees dans les handlers)*/
+etat_coureur_t etatCoureur;
+int id;
+
+/* HANDLERS */
+void handlerCoureur(int sig){
+    requete_t R;
+    reponse_t Rep;
+    etatCoureur = ABANDON;
+    R.type = PC_COURSE;
+    R.corps.dossard = getpid();
+    R.corps.etat = etatCoureur = ABANDON;
+    msgsnd(id, &R, sizeof(R.corps), 0);
+    msgrcv(id, &Rep, sizeof(Rep.corps), getpid(),0);
+    if(Rep.corps.compte_rendu == ABANDON_ACK){
+        fprintf(stderr,"Interruption par signal\n");
+        exit(sig);
+    }
+
+}
+
+int main(int argc, char * argv[]){
+
+    /* Capture des paramètres */
+    if(argc != 2){
+        fprintf(stderr,"Nombre de paramètres incorrect : ./%s <clé>\n", argv[0]);
+        exit(1);
+    }
+    int cle = atoi(argv[1]);
+
+    /* Déclarations et définitions sur la requete et la reponse */
+    requete_t R;
+    reponse_t Rep;
+    R.type = PC_COURSE;
+    R.corps.dossard = getpid();
+    R.corps.etat = EN_COURSE;
+    id = msgget(cle, 0666);
+
+    /* Si le coureur reçoit un signal de type SIGINT, il abandonne */
+    etatCoureur = EN_COURSE;
+    struct sigaction action;
+
+    action.sa_handler = handlerCoureur;
+    sigemptyset(&(action.sa_mask));
+    action.sa_flags = 0;
+
+    sigaction(SIGINT, &action, NULL);
+
+
+    while(etatCoureur == EN_COURSE){
+
+        if(etatCoureur == EN_COURSE){
+            /* Envoie d'une requete au PC course */
+            msgsnd(id, &R, sizeof(R.corps), 0);
+
+            /* Reception d'une réponse */
+            msgrcv(id, &Rep, sizeof(Rep.corps), getpid(),0);
+            R.corps.etat = etatCoureur = Rep.corps.etat;
+
+            system("clear");
+            messages_afficher_reponse( &Rep);
+            messages_afficher_parcours( &Rep);
+            messages_attendre_tour();
+        }
+    }
+}
